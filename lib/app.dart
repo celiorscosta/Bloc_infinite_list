7ac7e005c6d491c7505e82fee1e243b715b4f87c
celiorscosta <celiorscosta@hotmail.com>
2021-01-08 17:13:42 -0300

    final commit

---
+++ lib/app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:infinite_list/posts/view/post_page.dart';
+
+class App extends MaterialApp {
+  App() : super(home: PostsPage());
+}
---
+++ lib/posts/view/post_list.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_list/posts/bloc/bloc.dart';
+import 'package:infinite_list/posts/widgets/widgets.dart';
+
+class PostsList extends StatefulWidget {
+  @override
+  _PostsListState createState() => _PostsListState();
+}
+
+class _PostsListState extends State<PostsList> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+  PostBloc _postBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _postBloc = BlocProvider.of<PostBloc>(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PostBloc, PostState>(
+      builder: (context, state) {
+        if (state is PostInitial) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is PostFailure) {
+          return Center(
+            child: Text('failed to fetch posts'),
+          );
+        }
+        if (state is PostSuccess) {
+          if (state.posts.isEmpty) {
+            return Center(
+              child: Text('no posts'),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return index >= state.posts.length
+                  ? BottomLoader()
+                  : PostListItem(post: state.posts[index]);
+            },
+            itemCount: state.hasReachedMax
+                ? state.posts.length
+                : state.posts.length + 1,
+            controller: _scrollController,
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _postBloc.add(PostFetched());
+    }
+  }
+}
